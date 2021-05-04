0036077432e8e14620d9b2aeaef5e4d1aa8d35b2
YizheYang <58739358+YizheYang@users.noreply.github.com>
2021-05-04 22:49:09 +0800

    init

---
+++ NoteBook/app/src/main/java/com/github/YizheYang/SearchLayout.java
@@ -0,0 +1,19 @@
+package com.github.YizheYang;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+public class SearchLayout extends LinearLayout {
+
+	public SearchLayout(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+		LayoutInflater.from(context).inflate(R.layout.search, this);
+		Button search_Button = findViewById(R.id.search_Button);
+		EditText search_EditText = findViewById(R.id.search_EditText);
+		String text = search_EditText.getText().toString();
+	}
+}
